0be06e8626771198d2fcc63a729f2290573c15be
ihor.ustinchenko <ihor.ustinchenko@starwind.com>
2024-04-24 19:37:33 +0300

    3

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,6 +23,10 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+  const activeLink = (link: string) => {
+    const linksArr = link.split('/').filter(Boolean);
+    return linksArr.length > 1 ? pathname.includes(linksArr[1]) : pathname === link;
+  }
 
   return (
     <>
@@ -35,7 +39,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                ['bg-sky-100 text-blue-600']: pathname === link.href,
+                ['bg-sky-100 text-blue-600']: activeLink(link.href),
               },
             )}
           >
